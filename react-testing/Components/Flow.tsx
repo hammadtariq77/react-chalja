369421c2fda22053c0702e42be633384ee64e7ab
hammadtariq77 <hammadtariq7777@gmail.com>
2023-10-24 21:13:25 +0500

    Update Flow.tsx

--- react-testing/Components/Flow.tsx
+++ react-testing/Components/Flow.tsx
@@ -4,10 +4,10 @@ const Flow = () => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <div className="flex justify-between px-5 items-center bg-red-500 py-3 ">
+      <div className="flex justify-between px-5 items-center md:hidden bg-red-500 py-3 ">
         <div>logo</div>
 
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center ">
           <div>
             <button className="bg-blue-600 py-2 text-sm text-white px-4">
               Connet
@@ -38,6 +38,26 @@ const Flow = () => {
             </div>
           )}
         </div>
+      </div>
+      <div className="hidden md:flex px-32 items-center text-white justify-between   text-lg py-3 bg-sky-900 text-center">
+     <div className="flex gap-x-6">
+     <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <h1>hello</h1>
+        <div className="dropdown">
+  <label tabIndex={0} className=" ">Click</label>
+  <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</div>
+        <h1>hello</h1>
+        <h1>hello</h1>
+     </div>
+       <div>
+        <button className="bg-fuchsia-900 py-1  px-4">connect </button>
+       </div>
       </div>
     </>
   );
